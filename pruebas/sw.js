39e1b79ea7da586b5825226d5b8c5e385fa9053c
Soch86 <albertope86@gmail.com>
2026-09-05 11:21:27 +0200

    Add files via upload

--- pruebas/sw.js
+++ pruebas/sw.js
@@ -1,4 +1,4 @@
-const C='escribano-PRUEBAS-v11',F=['./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
+const C='escribano-PRUEBAS-v13',F=['./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(F)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x)))).then(()=>self.clients.claim()))});
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{if(r.ok&&new URL(e.request.url).origin===location.origin){const cp=r.clone();caches.open(C).then(c=>c.put(e.request,cp))}return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))))});
